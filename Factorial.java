c00d7a463305a9bd7d7c46ba433829d7259ba4ed
Pramodhadawale <117498038+Pramodhadawale@users.noreply.github.com>
2023-01-01 18:04:27 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+package Thursdaylab;
+
+import java.util.Scanner;
+
+public class Factorial {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i,fact=1,n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        n=sc.nextInt();
+        
+        for(i=1;i<=n;i++)
+        {
+        	fact=fact*i;
+        	sc.close();
+        }
+        
+        System.out.println("Factorial of"+n+" is: "+fact);
+	}
+
+}
---
+++ SumOfEvenNumbers.java
@@ -0,0 +1,27 @@
+package Thursdaylab;
+
+import java.util.Scanner;
+
+public class SumOfEvenNumbers {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i,n,sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter range");
+        n=sc.nextInt();
+        
+        for(i=2;i<=n;i=i+2)
+        {
+        
+        	sum=sum+i;
+        	sc.close();
+        	
+        }
+        
+	System.out.println("sum of all even numbers from 1 to "+n+" is :" +sum);
+
+	}
+
+}
